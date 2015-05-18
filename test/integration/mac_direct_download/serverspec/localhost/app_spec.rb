5074f3f69f5330d206b705ba139f9c2f3e54b4ba
Jonathan Hartman <j@p4nt5.com>
2015-05-18 14:18:55 -0700

    Add ServerSpec process checks

--- test/integration/mac_direct_download/serverspec/localhost/app_spec.rb
+++ test/integration/mac_direct_download/serverspec/localhost/app_spec.rb
@@ -8,4 +8,19 @@ describe 'Fantastical app' do
       expect(subject).to be_directory
     end
   end
+
+  describe command(
+    'osascript -e \'tell application "System Events" to get the name of the ' \
+    'login item "Divvy"\''
+  ) do
+    it 'indicates Fantastical is enabled' do
+      expect(subject.stdout.strip).to eq('Fantastical')
+    end
+  end
+
+  describe process('Fantastical 2') do
+    it 'is running' do
+      expect(subject).to be_running
+    end
+  end
 end
